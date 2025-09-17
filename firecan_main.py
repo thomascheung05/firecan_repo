7ec866e4cb46d64f6bd85553c2a43d7e28884bf6
thomascheung05 <thomascheung48@gmail.com>
2025-09-17 16:48:31 -0400

    put all that i had into fucntions

--- firecan_main.py
+++ firecan_main.py
@@ -9,89 +9,87 @@ work_dir  = Path.cwd()
 
 
 
-########## GETTING QUEBEC DATA AFTER 1976 ############################################################################
+##################### GET QC FIRE DATA ################################################################################################
+def get_qc_data(dataname, url, zipname, gpkgname):
+    savefolder = work_dir / dataname
+    zip_path = savefolder / zipname # zipname = "FEUX_PROV_GPKG.zip" OR "FEUX_PROV_GPKG.zip"
+    unzipped_file_path = savefolder / gpkgname # gpkgname = "FEUX_PROV.gpkg" OR "FEUX_ANCIENS_PROV.gpkg"
+    savepath = os.path.join(savefolder, zipname)
 
-qcfires_after76_savefolder = work_dir / "qcfires_after76"
-qcfires_after76_zip_path = qcfires_after76_savefolder / "FEUX_PROV_GPKG.zip"
-qcfires_after76_unzipped_file = qcfires_after76_savefolder / "FEUX_PROV.gpkg"
+    if not savefolder.exists():
+        savefolder.mkdir(parents=True, exist_ok=True)
+        print("Created folder:", savefolder)
+    else:
+        print("YAY Folder already exists:", savefolder)
+
+    if not zip_path.exists():
+        print(dataname, "is not downloaded, downloading now, this may take a few minutes")
+        response = requests.get(url)
+        with open(savepath, 'wb') as f:
+            f.write(response.content)
+        print("DONE Downloading the Data for", dataname)
+    else:
+        print("YAY ZIP", dataname,"already exists:", zip_path)
+    
+    if not unzipped_file_path.exists():
+        print("Unzipping the Data now")
+        with zipfile.ZipFile(savepath, 'r') as zip_ref:
+            zip_ref.extractall(savefolder)
+        print("DONE! unzipping the data, data is located in", savepath)
+    else:
+        print("YAY Unzipped",dataname, "file already exists:", unzipped_file_path)
+
+    return unzipped_file_path
+
+
 url_qcfires_after76 = 'https://diffusion.mffp.gouv.qc.ca/Diffusion/DonneeGratuite/Foret/PERTURBATIONS_NATURELLES/Feux_foret/02-Donnees/PROV/FEUX_PROV_GPKG.zip'
-qcfires_after76_savepath = os.path.join(qcfires_after76_savefolder, 'FEUX_PROV_GPKG.zip')
+qcfires_after76_zipname = "FEUX_PROV_GPKG.zip"
+qcfires_after76_gpkgname = "FEUX_PROV.gpkg"
+qcfires_after76_unzipped_file_path = get_qc_data("qcfires_after76", url_qcfires_after76, qcfires_after76_zipname, qcfires_after76_gpkgname)
+
+url_qcfires_after76 = 'https://diffusion.mffp.gouv.qc.ca/Diffusion/DonneeGratuite/Foret/PERTURBATIONS_NATURELLES/Feux_foret/02-Donnees/PROV/FEUX_ANCIENS_PROV_GPKG.zip'
+qcfires_after76_zipname = "FEUX_PROV_GPKG.zip"
+qcfires_after76_gpkgname = "FEUX_ANCIENS_PROV.gpkg"
+qcfires_before76_unzipped_file_path = get_qc_data("qcfires_before76", url_qcfires_after76, qcfires_after76_zipname, qcfires_after76_gpkgname)
+
+################################################################################################
 
 
-if not qcfires_after76_savefolder.exists():
-    qcfires_after76_savefolder.mkdir(parents=True, exist_ok=True)
-    print("Created folder:", qcfires_after76_savefolder)
-else:
-    print("YAY Folder already exists:", qcfires_after76_savefolder)
-
-if not qcfires_after76_zip_path.exists():
-    print('QC Fire Data after 76 is not downloaded, downloading now, this may take a few minutes')
-    response = requests.get(url_qcfires_after76)
-    with open(qcfires_after76_savepath, 'wb') as f:
-        f.write(response.content)
-    print("DONE! Downloading the Data")
-else:
-    print("YAY ZIP QC fires after 76 already exists:", qcfires_after76_zip_path)
-
-if not qcfires_after76_unzipped_file.exists():
-    print("Unzipping the Data now")
-    with zipfile.ZipFile(qcfires_after76_savepath, 'r') as zip_ref:
-        zip_ref.extractall(qcfires_after76_savefolder)
-    print("DONE! unzipping the data data is located in", qcfires_after76_savepath)
-else:
-    print("YAY Unzipped QC fires after 76 file already exists:", qcfires_after76_unzipped_file)
-
-#####################################################################################################################
-
-########## GETTING QUEBEC DATA BEFORE 1976 ############################################################################
-
-qcfires_before76_savefolder = work_dir / "qcfires_before76"
-qcfires_before76_zip_path = qcfires_before76_savefolder / "FEUX_PROV_GPKG.zip"
-qcfires_before76_unzipped_file = qcfires_before76_savefolder / "FEUX_ANCIENS_PROV.gpkg"
-url_qcfires_before76 = 'https://diffusion.mffp.gouv.qc.ca/Diffusion/DonneeGratuite/Foret/PERTURBATIONS_NATURELLES/Feux_foret/02-Donnees/PROV/FEUX_ANCIENS_PROV_GPKG.zip'
-qcfires_before76_savepath = os.path.join(qcfires_before76_savefolder, 'FEUX_PROV_GPKG.zip')
 
 
-if not qcfires_before76_savefolder.exists():
-    qcfires_before76_savefolder.mkdir(parents=True, exist_ok=True)
-    print("Created folder:", qcfires_before76_savefolder)
-else:
-    print("YAY Folder already exists:", qcfires_before76_savefolder)
-
-if not qcfires_before76_zip_path.exists():
-    print('QC Fire Data before 76 is not downloaded, downloading now, this may take a few minutes')
-    response = requests.get(url_qcfires_before76)
-    with open(qcfires_before76_savepath, 'wb') as f:
-        f.write(response.content)
-    print("DONE! Downloading the Data")
-else:
-    print("YAY ZIP QC fires before 76 already exists:", qcfires_before76_zip_path)
-
-if not qcfires_before76_unzipped_file.exists():
-    print("Unzipping the Data now")
-    with zipfile.ZipFile(qcfires_before76_savepath, 'r') as zip_ref:
-        zip_ref.extractall(qcfires_before76_savefolder)
-    print("DONE! unzipping the data data is located in", qcfires_before76_savepath)
-else:
-    print("YAY Unzipped QC fires before 76 file already exists:", qcfires_before76_unzipped_file)
-
-#####################################################################################################################
 
 
-######## LOADING IN AND MERGING QC FIRE DATA ##########################################################################################
-gdf_qcfire_after76 = gpd.read_file(qcfires_after76_unzipped_file, layer="feux_prov")
-gdf_qcfire_after76 = gdf_qcfire_after76.rename(columns={"geoc_fmj": "geoc"})
-gdf_qcfire_after76 = gdf_qcfire_after76.drop(columns=["perturb", "an_perturb", "part_str"])
 
-gdf_qcfire_before76 = gpd.read_file(qcfires_before76_unzipped_file, layer="feux_anciens_prov")
-gdf_qcfire_before76 = gdf_qcfire_before76.rename(columns={"geoc_fan": "geoc"})
 
-gdf_qcfire = gpd.GeoDataFrame(
-    pd.concat([gdf_qcfire_before76, gdf_qcfire_after76], ignore_index=True),
-    geometry="geometry"  # make sure the geometry column is correctly recognized
-)
 
-print(gdf_qcfire_after76.shape)   
-print(gdf_qcfire_before76.shape)     
-print(gdf_qcfire.shape)     
-########################################################################################################################
+
+
+
+
+
+
+############## LOAD IN AND MERGE BEFORE AND AFTER QC FIRE DATASET ##################################################################################
+def qc_loadmerge_data(afterpath, beforepath):
+    after = gpd.read_file(afterpath, layer="feux_prov")
+    after = after.rename(columns={"geoc_fmj": "geoc"})
+    after = after.drop(columns=["perturb", "an_perturb", "part_str"])
+
+    before = gpd.read_file(beforepath, layer="feux_anciens_prov")
+    before = before.rename(columns={"geoc_fan": "geoc"})
+
+    merged = gpd.GeoDataFrame(
+        pd.concat([before, after], ignore_index=True),
+        geometry="geometry"  
+    )
+    print(after.head())
+    print(before.head())
+
+    return merged
+
+gdf_qc_fires = qc_loadmerge_data(qcfires_after76_unzipped_file_path, qcfires_before76_unzipped_file_path)
+
+print(gdf_qc_fires.shape)
+
+
+    
+# ########################################################################################################################
