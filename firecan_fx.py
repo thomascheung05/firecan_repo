8c3c00be7f55bb2935aa315d6d39b57eae0b9a28
thomas cheung <tom@thomass-MacBook-Pro.local>
2025-10-05 21:12:16 -0400

    code is much cleaner, added a bunch of things including seperate watershed loading in and only loading in if watershed filter is selected, finialized teh new qc processing function, things work except the last 2 filting options

--- firecan_fx.py
+++ firecan_fx.py
@@ -40,93 +40,166 @@ def fx_scrape_donneqc(dataname, url, zipname, gpkgname): # Downloads the data (i
 
 
 def fx_qc_processfiredata(beforepath, afterpath): 
-    # Loads in before+after data, renames column to merge, merges, downlaods watersehd data, reproject data, merges fires and watershed data
-    # Skip this code and load data direclty IF it exists AND It is reprojected AND it has watershed column 
-    print("Loading in unprocessed data ... This may take a few minutes ...")
-    before_data = gpd.read_file(beforepath, layer= "feux_anciens_prov")
-    after_data = gpd.read_file(afterpath, layer= "feux_prov")
+    qc_processed_data_path = Path("qc_processed_data.gpkg")
+
+
+    if not qc_processed_data_path.exists():
+        # Loads in before+after data, renames column to merge, merges, downlaods watersehd data, reproject data, merges fires and watershed data
+        # Skip this code and load data direclty IF it exists AND It is reprojected AND it has watershed column 
+        print("Loading in unprocessed fire data ... This may take a few minutes ...")
+        before_data = gpd.read_file(beforepath, layer= "feux_anciens_prov")
+        after_data = gpd.read_file(afterpath, layer= "feux_prov")
 
 
 
-    print("Merging the data")
-    before_data = before_data.rename(columns={"geoc_fan": "geoc"})
-    after_data = after_data.rename(columns={"geoc_fmj": "geoc"})
-    after_data = after_data.drop(columns=["perturb", "an_perturb", "part_str"])
-    merged_data = gpd.GeoDataFrame(
-        pd.concat([before_data, after_data], ignore_index=True),
-        geometry="geometry"  
-    )
-    merged_data["an_origine"] = pd.to_numeric(merged_data["an_origine"], errors="coerce")
-    merged_data["superficie"] = pd.to_numeric(merged_data["superficie"], errors="coerce")
-    merged_data = merged_data.drop(columns=["exercice", "origine", "met_at_str", "shape_length", "shape_area"]) # might want shape length and share area later
+        print("Merging the data")
+        before_data = before_data.rename(columns={"geoc_fan": "geoc"})
+        after_data = after_data.rename(columns={"geoc_fmj": "geoc"})
+        after_data = after_data.drop(columns=["perturb", "an_perturb", "part_str"])
+        merged_data = gpd.GeoDataFrame(
+            pd.concat([before_data, after_data], ignore_index=True),
+            geometry="geometry"  
+        )
+        merged_data["an_origine"] = pd.to_numeric(merged_data["an_origine"], errors="coerce")
+        merged_data["superficie"] = pd.to_numeric(merged_data["superficie"], errors="coerce")
+        merged_data = merged_data.drop(columns=["exercice", "origine", "met_at_str", "shape_length", "shape_area"]) # might want shape length and share area later
 
 
 
-    print("Reprojecting the data")
-    is_wgs84 = merged_data.crs.to_epsg() == 4326
-    if is_wgs84:
-        print("The data is already in EPSG:4326 (WGS 84).")
+        print("Reprojecting Fire data")
+        is_wgs84 = merged_data.crs.to_epsg() == 4326
+        if is_wgs84:
+            print("The data is already in EPSG:4326 (WGS 84).")
+        else:
+            merged_data = merged_data.to_crs(epsg=4326)    
+
+
+        print("Saving processed QC data to load in later")
+        merged_data.to_file("qc_processed_data.gpkg", driver="GPKG")
+
+
     else:
-        print("Reprojecting Data")
-        merged_data = merged_data.to_crs(epsg=4326)    
+        print("The QC data is already processed, loading in now ...")
+        merged_data = gpd.read_file(qc_processed_data_path)
 
 
 
+    return merged_data
+
+
+
+
+def fx_get_watershed_data():
     print("Getting Watershed Data")
     url_watersheddata = 'https://stqc380donopppdtce01.blob.core.windows.net/donnees-ouvertes/Bassins_hydrographiques_multi_echelles/CE_bassin_multi.gdb.zip'
     watersheddata_zipname = "CE_bassin_multi.gdb.zip"
     watersheddata_fgdbname = "CE_bassin_multi.gdb"
+
     qcwatershed_unzipped_file_path = fx_scrape_donneqc("watershed_data", url_watersheddata, watersheddata_zipname, watersheddata_fgdbname)                   
-    
+        
     layers = fiona.listlayers(qcwatershed_unzipped_file_path)
     watershed_data = gpd.read_file(qcwatershed_unzipped_file_path, layer=layers[1])
     watershed_data = watershed_data.drop(columns=["NO_COURS_DEAU", "NOM_COURS_DEAU_MINUSCULE", "NIVEAU_BASSIN", "ECHELLE", "SUPERF_KM2", "NO_SEQ_BV_PRIMAIRE", "NOM_BV_PRIMAIRE", "NO_REG_HYDRO", "NOM_REG_HYDRO_ABREGE", "Shape_Length", "Shape_Area"]) # might want shape length and share area later
 
-
-
-    print("Reprojecting the data")
-    is_wgs84 = merged_data.crs.to_epsg() == 4326
-    if is_wgs84:
-        print("The data is already in EPSG:4326 (WGS 84).")
-    else:
-        print("Reprojecting Data")
-        merged_data = merged_data.to_crs(epsg=4326)  
-
+    print("Reprojecting Watershed data")
     is_wgs84 = watershed_data.crs.to_epsg() == 4326
     if is_wgs84:
         print("The data is already in EPSG:4326 (WGS 84).")
     else:
-        print("Reprojecting Data")
         watershed_data = watershed_data.to_crs(epsg=4326)  
-
-
-    # THIS TAKES SOOOOOOO LONG
-    # print("Adding watershed column to fires")
-    # merged_data = gpd.sjoin(
-    # merged_data, 
-    # watershed_data[['NOM_COURS_DEAU', 'geometry']],  # keep only watershed name & geometry
-    # how="left",                                      # keep all fires
-    # predicate="intersects"                           # or "within" if fires are fully inside watersheds
-    # )
-    # merged_data.drop(columns=["geometry"]).head(10).to_csv("merged_data.csv", index=False)
+    return watershed_data
 
 
 
+def fx_filter_fires_data(
+    fire_gdf,
+    min_year,
+    max_year,
+    min_size,
+    max_size,
+    distance_coords,
+    distance_radius,
+    watershed_name
+):
 
+    filtered_gdf = fire_gdf.copy()
+    conditions = []
+
+    if min_year  != "" and max_year  != "":
+        min_year = int(min_year)
+        max_year = int(max_year)
+        conditions.append((filtered_gdf["an_origine"] >= min_year) & (filtered_gdf["an_origine"] <= max_year))
+
+    if min_size  != "" or max_size  != "":
+        if min_size  != "":
+            min_size = int(min_size)
+        else:
+             min_size = 0
+        if min_size  != "":
+            max_size= int(max_size)
+        else:
+             min_size = 100000
+        conditions.append((filtered_gdf["superficie"] >= min_size) & (filtered_gdf["superficie"] <= max_size))
+
+    if distance_coords  != "" and distance_radius  != "":
+            lat, lon = map(float, distance_coords.split(","))
+            distance_radius = float(distance_radius)
+            user_point = gpd.GeoSeries([Point(lon, lat)], crs="EPSG:4326")
+            user_point_proj = user_point.to_crs(filtered_gdf.crs)
+            buffer_geom = user_point_proj.buffer(distance_radius)
+            conditions.append(filtered_gdf.geometry.intersects(buffer_geom.iloc[0]))
+
+    if watershed_name  != "":
+            gdf_watershed = fx_get_watershed_data()
+
+            selected_ws = gdf_watershed[gdf_watershed['NOM_COURS_DEAU'] == watershed_name]
+            if not selected_ws.empty:
+                ws_geom = selected_ws.geometry.unary_union
+                conditions.append(filtered_gdf.geometry.within(ws_geom))
+            else:
+                print(f"No watershed found with name '{watershed_name}'. Filter will be ignored.")
+
+    if conditions:
+        combined_mask = np.logical_and.reduce(conditions)
+        filtered_gdf = filtered_gdf[combined_mask]
+        
+    return filtered_gdf
 
  
 
 
+def fx_download_json(filtered_data):
+        geojson_data = json.loads(filtered_data.to_json())                    # Convert the filtered GeoDataFrame to GeoJSON
+        geojson_string = json.dumps(geojson_data) 
+    
+        geojson_buffer = io.BytesIO(geojson_string.encode('utf-8'))                         # Create an in-memory buffer to hold the file content
+        geojson_buffer.seek(0)
+        
+        return send_file(                                               # Send the file back to the browser as an attachment
+
+            geojson_buffer,
+            
+            mimetype='application/json',                    # CRITICAL: Change MIME type to 'application/json' or 'application/geo+json'
+            as_attachment=True,
+            download_name='firecan_filtered_data.geojson'                   # CRITICAL: Change file extension to '.geojson'
+
+        )
 
 
 
-url_qcfires_before76 = 'https://diffusion.mffp.gouv.qc.ca/Diffusion/DonneeGratuite/Foret/PERTURBATIONS_NATURELLES/Feux_foret/02-Donnees/PROV/FEUX_ANCIENS_PROV_GPKG.zip'
-qcfires_before76_zipname = "FEUX_PROV_GPKG.zip"
-qcfires_before76_gpkgname = "FEUX_ANCIENS_PROV.gpkg"
-qcfires_before76_unzipped_file_path = fx_scrape_donneqc("qcfires_before76", url_qcfires_before76, qcfires_before76_zipname, qcfires_before76_gpkgname)
-url_qcfires_after76 = 'https://diffusion.mffp.gouv.qc.ca/Diffusion/DonneeGratuite/Foret/PERTURBATIONS_NATURELLES/Feux_foret/02-Donnees/PROV/FEUX_PROV_GPKG.zip'
-qcfires_after76_zipname = "FEUX_PROV_GPKG.zip"
-qcfires_after76_gpkgname = "FEUX_PROV.gpkg"
-qcfires_after76_unzipped_file_path = fx_scrape_donneqc("qcfires_after76", url_qcfires_after76, qcfires_after76_zipname, qcfires_after76_gpkgname)
 
-fx_qc_processfiredata(qcfires_before76_unzipped_file_path,qcfires_after76_unzipped_file_path)
+def fx_download_csv(filtered_data):
+    csv_buffer = io.BytesIO()
+    filtered_data.to_csv(csv_buffer, index=False, encoding="utf-8")
+    csv_buffer.seek(0)
+    
+    return send_file(                                                   # Return the file as an attachment to the user's Downloads folder
+
+        csv_buffer,
+        mimetype='text/csv',
+        as_attachment=True,
+        download_name='firecan_filtered_data.csv'
+    ) 
+
+
+
--- firecan_main.py
+++ firecan_main.py
@@ -1,4 +1,4 @@
-from firecan_fx import fx_get_qc_data, fx_qc_firedata_merge, fx_qc_watersheddata_load, fx_filter_fires_data, fx_download_json, fx_download_csv, fx_load_and_reproject_data # type: ignore
+from firecan_fx import fx_scrape_donneqc,fx_qc_processfiredata,fx_filter_fires_data,fx_download_json,fx_download_csv, fx_get_watershed_data# type: ignore
 from flask import Flask, jsonify, request, send_file  # type: ignore
 import json
 from shapely.geometry import MultiPolygon 
@@ -16,22 +16,12 @@ qcfires_after76_gpkgname = "FEUX_PROV.gpkg"
 url_qcfires_before76 = 'https://diffusion.mffp.gouv.qc.ca/Diffusion/DonneeGratuite/Foret/PERTURBATIONS_NATURELLES/Feux_foret/02-Donnees/PROV/FEUX_ANCIENS_PROV_GPKG.zip'
 qcfires_before76_zipname = "FEUX_PROV_GPKG.zip"
 qcfires_before76_gpkgname = "FEUX_ANCIENS_PROV.gpkg"
+qcfires_before76_unzipped_file_path = fx_scrape_donneqc("qcfires_before76", url_qcfires_before76, qcfires_before76_zipname, qcfires_before76_gpkgname)
+qcfires_after76_unzipped_file_path = fx_scrape_donneqc("qcfires_after76", url_qcfires_after76, qcfires_after76_zipname, qcfires_after76_gpkgname)
 
-qcfires_before76_unzipped_file_path = fx_get_qc_data("qcfires_before76", url_qcfires_before76, qcfires_before76_zipname, qcfires_before76_gpkgname)
-qcfires_after76_unzipped_file_path = fx_get_qc_data("qcfires_after76", url_qcfires_after76, qcfires_after76_zipname, qcfires_after76_gpkgname)
+# Processing QC Fire data
+gdf_qc_fires = fx_qc_processfiredata(qcfires_before76_unzipped_file_path,qcfires_after76_unzipped_file_path)
 
-gdf_qc_fires_before = fx_load_and_reproject_data(qcfires_before76_unzipped_file_path,"feux_anciens_prov")
-gdf_qc_fires_after = fx_load_and_reproject_data(qcfires_after76_unzipped_file_path, "feux_prov")
-
-gdf_qc_fires = fx_qc_firedata_merge(gdf_qc_fires_after, gdf_qc_fires_before)
-
-
-
-url_watersheddata = 'https://stqc380donopppdtce01.blob.core.windows.net/donnees-ouvertes/Bassins_hydrographiques_multi_echelles/CE_bassin_multi.gdb.zip'
-watersheddata_zipname = "CE_bassin_multi.gdb.zip"
-watersheddata_fgdbname = "CE_bassin_multi.gdb"
-watersheddata_unzipped_file_path = fx_get_qc_data("watershed_data", url_watersheddata, watersheddata_zipname, watersheddata_fgdbname)
-gdf_qc_watershed = fx_qc_watersheddata_load(watersheddata_unzipped_file_path)
 
 print("Data pre-loading complete. The app is now ready to serve requests.")
 
@@ -64,7 +54,6 @@ def fx_main():
     # Filtering the data 
     filtered_data = fx_filter_fires_data(
                                             gdf_qc_fires,
-                                            gdf_qc_watershed,
                                             min_year=min_year,
                                             max_year=max_year,
                                             min_size=min_size,
