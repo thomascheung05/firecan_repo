ab1fc60ed58b1cb73f18eba0e874750f0200daac
thomascheung05 <thomascheung48@gmail.com>
2025-10-21 19:20:16 -0400

    should have fixed watershed filting and some structure around watershed data handling

--- firecan_fx.py
+++ firecan_fx.py
@@ -2,6 +2,7 @@ import zipfile
 import json
 import requests
 import geopandas as gpd
+from datetime import datetime
 from flask import request, send_file  
 import io
 from pathlib import Path
@@ -12,9 +13,15 @@ import numpy as np
 work_dir  = Path.cwd()
 
                                                                                         # A lot of the code here is pretty simple data manipulaiton mostly filtering
-                                                                                        # AI was used for the general "how do i do this" stuff but the code has been changed so much not a lot of still left over form the AI with the eception of the downloading functions, which are mostly AI as they are pretty straightforward and there wasnt much to change
+                                                                                        # AI was used for the general 'how do i do this' stuff but the code has been changed so much not a lot of still left over form the AI with the eception of the downloading functions, which are mostly AI as they are pretty straightforward and there wasnt much to change
                                                                                         # Almost all of the structure of this code is mine, like the download only if it doesnt exist and the process if it the processesed data doenst already exist
                                                                                         # AI showed me how to apply multiple filters at once which is a pretty integral part of the script
+
+
+
+
+
+
 
 def fx_scrape_donneqc(dataname, url, zipname, gpkgname):                                           # Scraps donne quebec to get quebef fire data, outputs the path to the data file
     savefolder = work_dir / dataname
@@ -22,97 +29,124 @@ def fx_scrape_donneqc(dataname, url, zipname, gpkgname):                        
     unzipped_file_path = savefolder / gpkgname                                                     # This differs between quebec fire data, and also watershed data set
 
     if not unzipped_file_path.exists():                                                            # Checks if the GPKG file exists, if not it will create a folder and downlaod it 
-        print("Data does not exist, Downloading now, this may take several minutes ...")
+        print('Data does not exist, Downloading now, this may take several minutes ...')
         savefolder.mkdir(parents=True, exist_ok=True)
-        print("Created folder:", savefolder)
+        print('Created folder:', savefolder)
         response = requests.get(url)                                                                # AI showed me how to do this
         with open(zip_path, 'wb') as f:
             f.write(response.content)
-        print("DONE Downloading the Data for", dataname) 
+        print('DONE Downloading the Data for', dataname) 
         with zipfile.ZipFile(zip_path, 'r') as zip_ref:                                            # The data come in a zipfile so must unzip it
             zip_ref.extractall(savefolder)
-        print("DONE! unzipping the data, data is located in", zip_path)
+        print('DONE! unzipping the data, data is located in', zip_path)
     else:                                                                                          # If it already exists we do nothing as we already created the path that needs to be returned outside of the IF 
-        print("..........Unzipped Data Exists For", dataname)                
+        print('....................This Dataset is already Downloaded', dataname)                
 
     return unzipped_file_path
 
 
+def fx_get_qc_watershed_data():                                                                                        # This function gets the watershed data by using the scrap donne quebec function, it then reads it in, drops some columns, and reprojects it
+    print('Getting Watershed Data')
+    url_watersheddata = 'https://stqc380donopppdtce01.blob.core.windows.net/donnees-ouvertes/Bassins_hydrographiques_multi_echelles/CE_bassin_multi.gdb.zip'
+    watersheddata_zipname = 'CE_bassin_multi.gdb.zip'
+    watersheddata_fgdbname = 'CE_bassin_multi.gdb'
+
+    qcwatershed_unzipped_file_path = fx_scrape_donneqc('qcwatershed_data', url_watersheddata, watersheddata_zipname, watersheddata_fgdbname)                   
+        
+    qc_processed_data_folder_path = work_dir / 'qc_processed_data'
+    qc_watershed_data_path = qc_processed_data_folder_path / 'qc_watershed_data.parquet'
+
+    if not qc_watershed_data_path.exists():
+        layers = fiona.listlayers(qcwatershed_unzipped_file_path)
+        watershed_data = gpd.read_file(qcwatershed_unzipped_file_path, layer=layers[1])
+        watershed_data = watershed_data.drop(columns=['NO_COURS_DEAU', 'NOM_COURS_DEAU_MINUSCULE', 'NIVEAU_BASSIN', 'ECHELLE', 'SUPERF_KM2', 'NO_SEQ_BV_PRIMAIRE', 'NOM_BV_PRIMAIRE', 'NO_REG_HYDRO', 'NOM_REG_HYDRO_ABREGE', 'Shape_Length', 'Shape_Area']) # might want shape length and share area later
+        
+        now = datetime.now().strftime('%H:%M:%S')
+        print('Reprojecting Watershed data Time:', now)
+        is_wgs84 = watershed_data.crs.to_epsg() == 4326
+        if is_wgs84:
+            print('The data is already in EPSG:4326 (WGS 84).')
+        else:
+            watershed_data = watershed_data.to_crs(epsg=4326)  
+
+        now = datetime.now().strftime('%H:%M:%S')
+        print('Saving Watershed Data for Later Time:',now)
+        watershed_data.to_parquet(qc_watershed_data_path)  
+        
+    else:
+        print('...............The Watershed data is already processed, loading in now ...')
+        watershed_data = gpd.read_parquet(qc_watershed_data_path)
+
+    return watershed_data
+
+        
 
 
 def fx_qc_processfiredata(beforepath, afterpath):                                                  # Loads in QC fire data (beofre and after), merges the two datasets, reprojects it, then saves it as a parquet so we only have to do this once 
 
-    qc_processed_data_folder_path = work_dir / "qc_processed_data"
-    qc_processed_data_path = qc_processed_data_folder_path / "qc_processed_data.parquet"
+    qc_processed_data_folder_path = work_dir / 'qc_processed_data'
+    qc_processed_data_path = qc_processed_data_folder_path / 'qc_processed_data.parquet'
 
     if not qc_processed_data_path.exists():
 
         qc_processed_data_folder_path.mkdir(parents=True, exist_ok=True)
-        print("Loading in unprocessed fire data ... This may take a few minutes ...")
-        before_data = gpd.read_file(beforepath, layer= "feux_anciens_prov")
-        after_data = gpd.read_file(afterpath, layer= "feux_prov")
+        print('Loading in unprocessed fire data ... This may take a few minutes ...')
+        before_data = gpd.read_file(beforepath, layer= 'feux_anciens_prov')
+        after_data = gpd.read_file(afterpath, layer= 'feux_prov')
 
+        after_data = after_data.drop(columns=['exercice', 'origine', 'met_at_str', 'shape_length', 'shape_area'])        # might want shape length and share area later
+        before_data = before_data.drop(columns=['exercice', 'origine', 'met_at_str', 'shape_length', 'shape_area'])        # might want shape length and share area later
 
-
-        print("Merging the data")
-        before_data = before_data.rename(columns={"geoc_fan": "geoc"})
-        after_data = after_data.rename(columns={"geoc_fmj": "geoc"})
-        after_data = after_data.drop(columns=["perturb", "an_perturb", "part_str"])
+        print('Merging the data')
+        before_data = before_data.rename(columns={'geoc_fan': 'geoc'})
+        after_data = after_data.rename(columns={'geoc_fmj': 'geoc'})
+        after_data = after_data.drop(columns=['perturb', 'an_perturb', 'part_str'])
         merged_data = gpd.GeoDataFrame(
             pd.concat([before_data, after_data], ignore_index=True),
-            geometry="geometry"  
+            geometry='geometry'  
         )
-        merged_data["an_origine"] = pd.to_numeric(merged_data["an_origine"], errors="coerce")
-        merged_data["superficie"] = pd.to_numeric(merged_data["superficie"], errors="coerce")
-        merged_data = merged_data.drop(columns=["exercice", "origine", "met_at_str", "shape_length", "shape_area"])        # might want shape length and share area later
+        merged_data['an_origine'] = pd.to_numeric(merged_data['an_origine'], errors='coerce')
+        merged_data['superficie'] = pd.to_numeric(merged_data['superficie'], errors='coerce')
 
 
 
-        print("Reprojecting Fire data")
+
+        now = datetime.now().strftime('%H:%M:%S')
+        print('Reprojecting Fire data (',now,')')
         is_wgs84 = merged_data.crs.to_epsg() == 4326
         if is_wgs84:
-            print("The data is already in EPSG:4326 (WGS 84).")
+            print('The data is already in EPSG:4326 (WGS 84).')
         else:
             merged_data = merged_data.to_crs(epsg=4326)    
+            now = datetime.now().strftime('%H:%M:%S')
+            print('The data has been reprojectd (',now,')')
 
 
-        print("Saving processed QC data to load in later")
+
+    
+
+
+
+        print('Saving processed QC data to load in later')
         merged_data.to_parquet(qc_processed_data_path)
 
     else:                                                                                                               # If there fire data is already processed we just load it in here
-        print("The QC data is already processed, loading in now ...")
+        print('...............The QC data is already processed, loading in now ...')
         merged_data = gpd.read_parquet(qc_processed_data_path)
+    
+    watershed_data = fx_get_qc_watershed_data()
+
+    return merged_data, watershed_data
 
 
-    return merged_data
 
 
-
-
-def fx_get_watershed_data():                                                                                        # This function gets the watershed data by using the scrap donne quebec function, it then reads it in, drops some columns, and reprojects it
-    print("Getting Watershed Data")
-    url_watersheddata = 'https://stqc380donopppdtce01.blob.core.windows.net/donnees-ouvertes/Bassins_hydrographiques_multi_echelles/CE_bassin_multi.gdb.zip'
-    watersheddata_zipname = "CE_bassin_multi.gdb.zip"
-    watersheddata_fgdbname = "CE_bassin_multi.gdb"
-
-    qcwatershed_unzipped_file_path = fx_scrape_donneqc("watershed_data", url_watersheddata, watersheddata_zipname, watersheddata_fgdbname)                   
-        
-    layers = fiona.listlayers(qcwatershed_unzipped_file_path)
-    watershed_data = gpd.read_file(qcwatershed_unzipped_file_path, layer=layers[1])
-    watershed_data = watershed_data.drop(columns=["NO_COURS_DEAU", "NOM_COURS_DEAU_MINUSCULE", "NIVEAU_BASSIN", "ECHELLE", "SUPERF_KM2", "NO_SEQ_BV_PRIMAIRE", "NOM_BV_PRIMAIRE", "NO_REG_HYDRO", "NOM_REG_HYDRO_ABREGE", "Shape_Length", "Shape_Area"]) # might want shape length and share area later
-
-    print("Reprojecting Watershed data")
-    is_wgs84 = watershed_data.crs.to_epsg() == 4326
-    if is_wgs84:
-        print("The data is already in EPSG:4326 (WGS 84).")
-    else:
-        watershed_data = watershed_data.to_crs(epsg=4326)  
-    return watershed_data
 
 
 
 def fx_filter_fires_data(                                                                             # This is the function that actually filters the data, it takes in fire data (and watershed but removed it cause brocken for now), as well as user inputs for filtering 
-    fire_gdf,                                                                                         # Nore sure if i already mentioned this but i used AI to help apply all the filters at once / apply only the ones the user inputed
+    fire_gdf,      
+    watershed_data,                                                                                   # Nore sure if i already mentioned this but i used AI to help apply all the filters at once / apply only the ones the user inputed
     min_year,
     max_year,
     min_size,
@@ -125,46 +159,45 @@ def fx_filter_fires_data(                                                       
     filtered_gdf = fire_gdf.copy()
     conditions = []                                                                                     # This is a list of the filtering conditions so they can all be applied at once 
 
-    if min_year  != "" or max_year  != "":                                                                  # This IFs for all of these check if the filtering box on the site has a value inputed in it for this one and the next one we use OR becuase wse want the use to be able to input only a max or a min and not HAVE to input both 
-        if min_year  != "":
+    if min_year  != '' or max_year  != '':                                                                  # This IFs for all of these check if the filtering box on the site has a value inputed in it for this one and the next one we use OR becuase wse want the use to be able to input only a max or a min and not HAVE to input both 
+        if min_year  != '':
             min_year = int(min_year)
         else:                                                                                               # If this field was empty (and the other was not as the IF is running) then we assing the min year to 0, if we dont do this it tryus to convert NULL to an int
              min_year = 0
-        if max_year  != "":
+        if max_year  != '':
             max_year= int(max_year)
         else:
              max_year = 100000
-        conditions.append((filtered_gdf["an_origine"] >= min_year) & (filtered_gdf["an_origine"] <= max_year))      # This appends the condition to the filtering list to be applied later 
+        conditions.append((filtered_gdf['an_origine'] >= min_year) & (filtered_gdf['an_origine'] <= max_year))      # This appends the condition to the filtering list to be applied later 
 
-    if min_size  != "" or max_size  != "":
-        if min_size  != "":
+    if min_size  != '' or max_size  != '':
+        if min_size  != '':
             min_size = int(min_size)
         else:
              min_size = 0
-        if min_size  != "":
+        if min_size  != '':
             max_size= int(max_size)
         else:
              min_size = 100000
-        conditions.append((filtered_gdf["superficie"] >= min_size) & (filtered_gdf["superficie"] <= max_size))
+        conditions.append((filtered_gdf['superficie'] >= min_size) & (filtered_gdf['superficie'] <= max_size))
 
-    if distance_coords  != "" and distance_radius  != "":                                                     # The is the distance radius filtering that only selects fires within a radius of a point 
-            lat, lon = map(float, distance_coords.split(","))       
+    if distance_coords  != '' and distance_radius  != '':                                                     # The is the distance radius filtering that only selects fires within a radius of a point 
+            lat, lon = map(float, distance_coords.split(','))       
             distance_radius = float(distance_radius)
-            user_point = gpd.GeoSeries([Point(lon, lat)], crs="EPSG:4326")                                    # This creates the point based ont he user inputed coords 
+            user_point = gpd.GeoSeries([Point(lon, lat)], crs='EPSG:4326')                                    # This creates the point based ont he user inputed coords 
             utm_crs = user_point.estimate_utm_crs()                             
             user_point_m = user_point.to_crs(utm_crs)                                                             # This chanes the point to a projection that makes sense for its locatoin, we cannot have it in EPSG: 4326 becuase this projection cant measure distances in metres only in degrees 
             buffer_m = user_point_m.buffer(distance_radius)                                                         # This creates a buffer around the point with a radius that the user inputed 
-            buffer_deg = buffer_m.to_crs("EPSG:4326")                                                             # Here we reproject the buffer back to EPSG:4326 so leaflet can display it, AI helped me construct this process but the logic behind it was mine, at first it wanted me to do a very roundabout way
+            buffer_deg = buffer_m.to_crs('EPSG:4326')                                                             # Here we reproject the buffer back to EPSG:4326 so leaflet can display it, AI helped me construct this process but the logic behind it was mine, at first it wanted me to do a very roundabout way
             conditions.append(filtered_gdf.geometry.intersects(buffer_deg.iloc[0]))
 
-    if watershed_name  != "":                                                                             # This shit dont work
-            gdf_watershed = fx_get_watershed_data()
-            selected_ws = gdf_watershed[gdf_watershed['NOM_COURS_DEAU'] == watershed_name]
+    if watershed_name  != '':                                                                             # This shit dont work
+            selected_ws = watershed_data[watershed_data['NOM_COURS_DEAU'] == watershed_name]
             if not selected_ws.empty:
                 ws_geom = selected_ws.geometry.unary_union                                                  # This from AI 
                 conditions.append(filtered_gdf.geometry.within(ws_geom))
             else:
-                print(f"No watershed found with name '{watershed_name}'. Filter will be ignored.")
+                print(f'No watershed found with name "{watershed_name}". Filter will be ignored.')
 
     if conditions:
         combined_mask = np.logical_and.reduce(conditions)                                                         # The combined filtered conditions
@@ -198,7 +231,7 @@ def fx_download_json(filtered_data):                                            
 def fx_download_csv(filtered_data):     # Exact same thing as the last function but downloads as csv        
 
     csv_buffer = io.BytesIO()
-    filtered_data.drop(columns=['geometry'], errors='ignore').to_csv(csv_buffer, index=False, encoding="utf-8")         # Drops geom column here too 
+    filtered_data.drop(columns=['geometry'], errors='ignore').to_csv(csv_buffer, index=False, encoding='utf-8')         # Drops geom column here too 
 
     csv_buffer.seek(0)
     
--- firecan_main.py
+++ firecan_main.py
@@ -1,27 +1,26 @@
-from firecan_fx import fx_scrape_donneqc,fx_qc_processfiredata,fx_filter_fires_data,fx_download_json,fx_download_csv, fx_get_watershed_data# type: ignore
-from flask import Flask, jsonify, request# type: ignore
-import json
+from firecan_fx import fx_scrape_donneqc,fx_qc_processfiredata,fx_filter_fires_data,fx_download_json,fx_download_csv# type: ignore
+from flask import Flask, request # type: ignore
+from datetime import datetime
 
 
 
 
-
-print("Starting data pre-loading. This may take a few minutes...")                                      # This section here loads in the data, it uses the scrap donne quebec function and the process qc fire data fuction
+print('Starting data pre-loading. This may take a few minutes...')                                      # This section here loads in the data, it uses the scrap donne quebec function and the process qc fire data fuction
 
 url_qcfires_after76 = 'https://diffusion.mffp.gouv.qc.ca/Diffusion/DonneeGratuite/Foret/PERTURBATIONS_NATURELLES/Feux_foret/02-Donnees/PROV/FEUX_PROV_GPKG.zip'
-qcfires_after76_zipname = "FEUX_PROV_GPKG.zip"                                                          # In this case both zip names are the same but must still specify it in the fuctino so we can use the fuction for other datasets like the watershed data
-qcfires_after76_gpkgname = "FEUX_PROV.gpkg"
+qcfires_after76_zipname = 'FEUX_PROV_GPKG.zip'                                                          # In this case both zip names are the same but must still specify it in the fuctino so we can use the fuction for other datasets like the watershed data
+qcfires_after76_gpkgname = 'FEUX_PROV.gpkg'
 url_qcfires_before76 = 'https://diffusion.mffp.gouv.qc.ca/Diffusion/DonneeGratuite/Foret/PERTURBATIONS_NATURELLES/Feux_foret/02-Donnees/PROV/FEUX_ANCIENS_PROV_GPKG.zip'
-qcfires_before76_zipname = "FEUX_PROV_GPKG.zip"
-qcfires_before76_gpkgname = "FEUX_ANCIENS_PROV.gpkg"
-qcfires_before76_unzipped_file_path = fx_scrape_donneqc("qcfires_before76", url_qcfires_before76, qcfires_before76_zipname, qcfires_before76_gpkgname)
-qcfires_after76_unzipped_file_path = fx_scrape_donneqc("qcfires_after76", url_qcfires_after76, qcfires_after76_zipname, qcfires_after76_gpkgname)
+qcfires_before76_zipname = 'FEUX_PROV_GPKG.zip'
+qcfires_before76_gpkgname = 'FEUX_ANCIENS_PROV.gpkg'
+qcfires_before76_unzipped_file_path = fx_scrape_donneqc('qcfires_before76', url_qcfires_before76, qcfires_before76_zipname, qcfires_before76_gpkgname)
+qcfires_after76_unzipped_file_path = fx_scrape_donneqc('qcfires_after76', url_qcfires_after76, qcfires_after76_zipname, qcfires_after76_gpkgname)
 
 
-gdf_qc_fires = fx_qc_processfiredata(qcfires_before76_unzipped_file_path,qcfires_after76_unzipped_file_path)
+gdf_qc_fires, gdf_qc_watershed_data = fx_qc_processfiredata(qcfires_before76_unzipped_file_path,qcfires_after76_unzipped_file_path)
 
 
-print("Data pre-loading complete. The app is now ready to serve requests.")
+print('Data pre-loading complete. The app is now ready to serve requests.')
 
 
 
@@ -47,9 +46,10 @@ def fx_main():                                                                  
     is_download_requested = request.args.get('download', '0') == '1'                                      # Checks if we should be displaying data or downloading it
     jsondownlaod = request.args.get('jsondownload', None)
 
-    print("Filtering Data")                                                                                 # Uses the filtering fire function to return a dataset with only the fires the user wants 
+    print('Filtering Data')                                                                                 # Uses the filtering fire function to return a dataset with only the fires the user wants 
     filtered_data = fx_filter_fires_data(
                                             gdf_qc_fires,
+                                            gdf_qc_watershed_data,
                                             min_year=min_year,
                                             max_year=max_year,
                                             min_size=min_size,
@@ -58,22 +58,24 @@ def fx_main():                                                                  
                                             distance_radius=distance_radius,
                                             watershed_name=watershed_name
                                         )
-    print("Done Filtering Data")
+    print('Done Filtering Data')
 
     
 
     
     if is_download_requested:                                                                                 # If downlaod request is tru we are going to run one of the downloading fucntions
-        if jsondownlaod == "true":
+        if jsondownlaod == 'true':
             return fx_download_json(filtered_data)
         else:
             return fx_download_csv(filtered_data)
-    else:                                                                                                     # IF download request is not true we are going to convert ot geojson and return it (send it) to my java script
-        print("Converting to geojson")
-        geojson_data = json.loads(filtered_data.to_json())                                                    # BOTTLENECK
-        print("Done Converting to geojson")
-
-        return jsonify(geojson_data)                                      
+    else:    
+        now = datetime.now().strftime('%H:%M:%S')                                                                                                 # IF download request is not true we are going to convert ot geojson and return it (send it) to my java script
+        print('Converting to geojson (',now,')')
+        geojson_str = filtered_data.to_json()
+        now = datetime.now().strftime('%H:%M:%S')                                                     # BOTTLENECK
+        print('Done Converting to geojson (',now,')')
+        return geojson_str
+                                   
 
 @app.route('/')
 def serve_html():
