0399dda9b275914d4c36ba589ef937382bdc6083
Matthew Cheung <thomascheung48@gmail.com>
2025-11-02 19:29:49 -0500

    replace ngrok with instatunnel

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 cd ~/.Thomas/firecan_repo
-gnome-terminal -- ./venv/bin/ngrok http 5000
+gnome-terminal -- ./venv/bin/instatunnel connect 5000 --subdomain firecan
 gnome-terminal -- ./venv/bin/python firecan_main.py
