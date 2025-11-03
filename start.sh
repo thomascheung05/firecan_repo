d2b7568ab134a5b6c95592c299e3ee1f003f8e4f
thomascheung05 <thomascheung48@gmail.com>
2025-11-02 20:01:48 -0500

    1

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
-cd ~/.Thomas/firecan_repo
-gnome-terminal -- ./venv/bin/instatunnel connect 5000 --subdomain firecan
+cd ~/Thomas/firecan_repo
+gnome-terminal -- ./venv/bin/instatunnel connect 5000 --subdomain firecan --api-key it_6c1e405459eb827f455357a73338031c99a207936e1a0f8b45e5c507deb8
 gnome-terminal -- ./venv/bin/python firecan_main.py
+while true; do
+  curl -s https://firecan.instatunnel.my/ > /dev/null
+  sleep 60  # ping every 4 minutes
+done
